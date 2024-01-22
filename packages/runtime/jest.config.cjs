af136d74162645f6d099ae70817861a1128c9806
markolazic01 <markolazic01@gmail.com>
2024-01-22 12:44:34 +0100

    fix: increase test timeout

--- packages/runtime/jest.config.cjs
+++ packages/runtime/jest.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     rootDir: './',
     moduleDirectories: ["node_modules", "src"],
     extensionsToTreatAsEsm: ['.ts'],
-    testTimeout: 50_000,
+    testTimeout: 70_000,
     setupFilesAfterEnv: ["jest-expect-message"],
     moduleNameMapper: {
         '../../../node_modules/snarkyjs/dist/(.*)': '<rootDir>/node_modules/snarkyjs/dist/$1',
